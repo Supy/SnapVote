9a40557e1c13cd9f555fb2670c46026a655f7fe6
Ben <benmeier42@gmail.com>
2013-08-12 21:15:06 +0200

    Finished off blob detection. need to isolate next

--- SnapVote/src/main/java/uct/snapvote/filter/BlobDetectorFilter.java
+++ SnapVote/src/main/java/uct/snapvote/filter/BlobDetectorFilter.java
@@ -2,6 +2,8 @@ package uct.snapvote.filter;
 
 import android.util.Log;
 
+import java.util.concurrent.Callable;
+
 import uct.snapvote.ImageByteBuffer;
 
 /**
@@ -15,52 +17,107 @@ public class BlobDetectorFilter extends BaseRegionFilter {
         super(source, null);
     }
 
+    public class Blob
+    {
+        public int minx, miny, maxx, maxy;
+        public char label;
+    }
+
     @Override
     public void run(){
-        char labelBuffer[] = new char[x2*y2];
-        char labelTable[] = new char[MAX_CHAR_VAL];
 
-        char currentLabel = 1;
+        // this contains the labels for the pixels. 0 = no label
+        char[] pixelLabels = new char[source.getHeight() * source.getWidth()];
 
-        for(int y = 1; y < y2-1; y++){
-            for(int x = 1; x < x2-1; x++){
-                byte pixel = source.get(x,y);
+        char[] labelTable = new char[Character.MAX_VALUE];
 
-                if(pixel == 0){
-                    int topRow = (x2 * (y-1)) + x;
+        char currentlabel = 1;
 
-                    // Max char value;
-                    char min = MAX_CHAR_VAL;
+        // loop through all pixels
+        for(int y=1;y<source.getHeight()-1;y++)
+        {
+            for(int x=1;x<source.getWidth()-1;x++)
+            {
+                int px = source.get(x,y) & 0xFF;
 
-                    // Get neighbour labels
-                    char top = labelBuffer[topRow];
-                    char left = labelBuffer[topRow+x2-1];
+                // can this pixel be labeled?
+                if (px == 0)
+                {
+                    // check top and left pixel labels
+                    char north = pixelLabels[(y-1) * source.getWidth() + x];
+                    char west = pixelLabels[y * source.getWidth() + (x-1)];
+                    //char northwest = pixelLabels[(y-1) * source.getWidth() + (x-1)];
+                    //char northeast = pixelLabels[(y-1) * source.getWidth() + (x+1)];
 
-                    if(top != 0 && top < min)
-                        min = top;
+                    char min = Character.MAX_VALUE;
+                    if(north > 0 && north < min) min = north;
+                    if(west > 0 && west < min) min = west;
+                    //if(northwest > 0 && northwest < min) min = northwest;
+                   // if(northeast > 0 && northeast < min) min = northeast;
 
-                    if(left != 0 && left < min)
-                        min = left;
+                    // if neither were labeled
+                    if (min == Character.MAX_VALUE)
+                    {
+                        // label the pixel with currentlabel
+                        pixelLabels[y * source.getWidth() + x] = currentlabel;
+                        labelTable[currentlabel] = currentlabel;
+                        // increment current label
+                        currentlabel++;
+                    }
+                    else
+                    {
+                        pixelLabels[y * source.getWidth() + x] = min;
 
-                    int currentIndex = topRow + x2;
-
-                    if(min == MAX_CHAR_VAL){
-                        labelBuffer[currentIndex] = currentLabel;
-                        labelTable[currentLabel] = currentLabel;
-                        currentLabel++;
-                    }else{
-                        labelBuffer[currentIndex] = min;
-
-                        if(top != 0)
-                            labelTable[top] = min;
-
-                        if(left != 0)
-                            labelTable[left] = min;
+                        if (north > 0) labelTable[north] = min;
+                        if (west > 0) labelTable[west] = min;
                     }
                 }
             }
         }
 
-        Log.d("uct.snapvote", "Labels: "+(int)currentLabel+".");
+        char maxlabelcount = currentlabel;
+
+
+        // for each label, decay it
+        for (int i = maxlabelcount-1; i >0;i--)
+        {
+            if (labelTable[i] != i)
+            {
+                int l = i;
+                while (l != labelTable[l]) l = labelTable[l];
+                labelTable[i] = (char)l;
+            }
+        }
+
+        char newlabel = 0;
+        for(int i=0;i< maxlabelcount-1;i++)
+        {
+            if ( labelTable[i] == i) labelTable[i] = newlabel++;
+            else labelTable[i] = labelTable[labelTable[i]];
+        }
+
+        byte [] colours = new byte[]{
+                (byte) 31,
+                (byte) 63,
+                (byte) 95,
+                (byte) 127,
+                (byte) 159,
+                (byte) 191,
+                (byte) 223
+        };
+
+        for(int y=1;y<source.getHeight()-1;y++)
+        {
+            for(int x=1;x<source.getWidth()-1;x++)
+            {
+                char l = pixelLabels[y * source.getWidth() + x];
+                char rootlabel = labelTable[l];
+
+                source.set(x,y, colours[rootlabel % 7]);
+            }
+        }
+
+
+        //Log.d("uct.snapvote", "Labels: "+(int)count+".");
     }
 }
