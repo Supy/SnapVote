0e94e11be64287ae344a03cc680bdb38e4f66536
Ben <benmeier42@gmail.com>
2013-08-12 22:32:16 +0200

    draw square around blobs

--- SnapVote/src/main/java/uct/snapvote/filter/BlobDetectorFilter.java
+++ SnapVote/src/main/java/uct/snapvote/filter/BlobDetectorFilter.java
@@ -2,6 +2,8 @@ package uct.snapvote.filter;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import uct.snapvote.ImageByteBuffer;
@@ -17,10 +19,27 @@ public class BlobDetectorFilter extends BaseRegionFilter {
         super(source, null);
     }
 
-    public class Blob
+    static class Blob
     {
-        public int minx, miny, maxx, maxy;
-        public char label;
+        public int xMin;
+        public int xMax;
+        public int yMin;
+        public int yMax;
+        public int mass;
+
+        public Blob(int xMin, int xMax, int yMin, int yMax, int mass)
+        {
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.yMax = yMax;
+            this.mass = mass;
+        }
+
+        public String toString()
+        {
+            return String.format("X: %4d -> %4d, Y: %4d -> %4d, mass: %6d", xMin, xMax, yMin, yMax, mass);
+        }
     }
 
     @Override
@@ -30,6 +49,11 @@ public class BlobDetectorFilter extends BaseRegionFilter {
         char[] pixelLabels = new char[source.getHeight() * source.getWidth()];
 
         char[] labelTable = new char[Character.MAX_VALUE];
+        char[] xMinTable = new char[Character.MAX_VALUE];
+        char[] xMaxTable = new char[Character.MAX_VALUE];
+        char[] yMinTable = new char[Character.MAX_VALUE];
+        char[] yMaxTable = new char[Character.MAX_VALUE];
+        char[] massTable = new char[Character.MAX_VALUE];
 
         char currentlabel = 1;
 
@@ -38,6 +62,8 @@ public class BlobDetectorFilter extends BaseRegionFilter {
         {
             for(int x=1;x<source.getWidth()-1;x++)
             {
+                pixelLabels[y * source.getWidth() + x] = 0;
+
                 int px = source.get(x,y) & 0xFF;
 
                 // can this pixel be labeled?
@@ -46,14 +72,14 @@ public class BlobDetectorFilter extends BaseRegionFilter {
                     // check top and left pixel labels
                     char north = pixelLabels[(y-1) * source.getWidth() + x];
                     char west = pixelLabels[y * source.getWidth() + (x-1)];
-                    //char northwest = pixelLabels[(y-1) * source.getWidth() + (x-1)];
-                    //char northeast = pixelLabels[(y-1) * source.getWidth() + (x+1)];
+                    char northwest = pixelLabels[(y-1) * source.getWidth() + (x-1)];
+                    char northeast = pixelLabels[(y-1) * source.getWidth() + (x+1)];
 
                     char min = Character.MAX_VALUE;
-                    if(north > 0 && north < min) min = north;
+                    if(north > 0) min = north;
                     if(west > 0 && west < min) min = west;
-                    //if(northwest > 0 && northwest < min) min = northwest;
-                   // if(northeast > 0 && northeast < min) min = northeast;
+                    if(northwest > 0 && northwest < min) min = northwest;
+                    if(northeast > 0 && northeast < min) min = northeast;
 
                     // if neither were labeled
                     if (min == Character.MAX_VALUE)
@@ -61,6 +87,15 @@ public class BlobDetectorFilter extends BaseRegionFilter {
                         // label the pixel with currentlabel
                         pixelLabels[y * source.getWidth() + x] = currentlabel;
                         labelTable[currentlabel] = currentlabel;
+
+                        yMinTable[currentlabel] = (char)y;
+                        yMaxTable[currentlabel] = (char)y;
+
+                        xMinTable[currentlabel] = (char)x;
+                        xMaxTable[currentlabel] = (char)x;
+
+                        massTable[currentlabel] = 1;
+
                         // increment current label
                         currentlabel++;
                     }
@@ -68,8 +103,21 @@ public class BlobDetectorFilter extends BaseRegionFilter {
                     {
                         pixelLabels[y * source.getWidth() + x] = min;
 
-                        if (north > 0) labelTable[north] = min;
-                        if (west > 0) labelTable[west] = min;
+                        yMaxTable[min] = (char)y;
+                        massTable[min]++;
+
+                        if( x< xMinTable[min]) xMinTable[min] = (char)x;
+                        if( x> xMaxTable[min]) xMaxTable[min] = (char)x;
+
+
+                        if (north > 0)
+                        {
+                            labelTable[north] = min;
+                        }
+                        if (west > 0)
+                        {
+                            labelTable[north] = min;
+                        }
                     }
                 }
             }
@@ -77,8 +125,30 @@ public class BlobDetectorFilter extends BaseRegionFilter {
 
         char maxlabelcount = currentlabel;
 
+        List<Blob> blobs = new ArrayList<Blob>();
 
         // for each label, decay it
+        for (int i = maxlabelcount-1; i >0;i--)
+        {
+            if (labelTable[i] != i)
+            {
+                if( xMaxTable[i] > xMaxTable[labelTable[i]]) xMaxTable[labelTable[i]] = xMaxTable[i];
+                if( xMinTable[i] < xMinTable[labelTable[i]]) xMinTable[labelTable[i]] = xMinTable[i];
+                if( yMaxTable[i] > yMaxTable[labelTable[i]]) yMaxTable[labelTable[i]] = yMaxTable[i];
+                if( yMinTable[i] < yMinTable[labelTable[i]]) yMinTable[labelTable[i]] = yMinTable[i];
+                massTable[labelTable[i]] += massTable[i];
+
+                int l = i;
+                while (l != labelTable[l]) l = labelTable[l];
+                labelTable[i] = (char)l;
+            }
+            else
+            {
+                Blob blob = new Blob(xMinTable[i], xMaxTable[i], yMinTable[i], yMaxTable[i], massTable[i]);
+                blobs.add(blob);
+            }
+        }
+
         for (int i = maxlabelcount-1; i >0;i--)
         {
             if (labelTable[i] != i)
@@ -116,6 +186,24 @@ public class BlobDetectorFilter extends BaseRegionFilter {
                 source.set(x,y, colours[rootlabel % 7]);
             }
         }
+
+        for(Blob b : blobs)
+        {
+            for(int y=b.yMin;y< b.yMax;y++)
+            {
+                source.set(b.xMin,y,(byte)255);
+                source.set(b.xMax,y,(byte)255);
+
+                if( y ==b.yMin  || y == (b.yMax-1))
+                {
+                    for(int x=b.xMin;x< b.xMax;x++)
+                    {
+                        source.set(x,y,(byte)255);
+                    }
+                }
+            }
+        }
+
 
 
         //Log.d("uct.snapvote", "Labels: "+(int)count+".");
