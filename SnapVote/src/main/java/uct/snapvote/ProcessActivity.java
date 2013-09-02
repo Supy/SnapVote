2bc44492dc7d9b0afc92de86e4900d5c3764001d
Ben Meier <benmeier42@gmail.com>
2013-09-03 00:16:43 +0200

    Added yes no box for cancelling the processing async task

--- SnapVote/src/main/java/uct/snapvote/ProcessActivity.java
+++ SnapVote/src/main/java/uct/snapvote/ProcessActivity.java
@@ -1,7 +1,10 @@
 package uct.snapvote;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
+import android.preference.DialogPreference;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -17,6 +20,8 @@ public class ProcessActivity extends Activity {
     TextView tvConsole;
     ProgressBar pbMainProgress;
 
+    SquareDetectionAsyncTask processingTask = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +36,41 @@ public class ProcessActivity extends Activity {
         pbMainProgress = (ProgressBar) findViewById(R.id.progressBar);
 
         // run shit
-        SquareDetectionAsyncTask task = new SquareDetectionAsyncTask(this);
-        task.execute(imageUri);
+        processingTask = new SquareDetectionAsyncTask(this);
+        processingTask.execute(imageUri);
 
 
 
     }
 
+    @Override
+    // When back button is pressed, the user may want to cancel processing
+    public void onBackPressed() {
+        // Create an alert box with Yes / No buttons
+        AlertDialog.Builder  yesNoBox = new AlertDialog.Builder(this);
+        yesNoBox.setIcon(R.drawable.ic_launcher);
+        yesNoBox.setMessage("Are you sure you want to exit image processing?");
+        yesNoBox.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // If Yes is pressed,
+                // cancel the async task if it is running
+                if(processingTask == null || !processingTask.isCancelled())
+                {
+                    processingTask.cancel(true);
+                }
+
+                // then exit the activity
+                ProcessActivity.this.finish();
+            }
+        });
+        yesNoBox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // If no was pressed, just continue
+            }
+        });
+        yesNoBox.show();
+    }
     
 }
