a4f8e786ac98138fb863062514b631229976dc9d
Ben Meier <benmeier42@gmail.com>
2013-09-22 23:28:47 +0200

    Commentations.

--- SnapVote/src/main/java/uct/snapvote/util/DebugTimer.java
+++ SnapVote/src/main/java/uct/snapvote/util/DebugTimer.java
@@ -1,38 +1,41 @@
 package uct.snapvote.util;
 
-import android.util.Log;
-
 /**
- * Created by Ben on 8/4/13.
+ * Simple wrapper around a stopwatch pattern. Provides easy time toString functionality.
  */
 public class DebugTimer {
     long starttime, splittime;
 
+    // start timing on construct
     public DebugTimer(){
         starttime = System.currentTimeMillis();
         splittime = starttime;
     }
 
+    // set the split time
     public void split() {
         splittime = System.currentTimeMillis();
     }
 
+    // get milliseconds
     long splitMilliseconds()
     {
         return System.currentTimeMillis() - splittime;
     }
 
+    // get total milliseconds
     long totalMilliseconds()
     {
         return System.currentTimeMillis() - starttime;
     }
 
-
+    // to string of elapsed time between now and splittime
     public String toStringSplit() {
         long elapsed = splitMilliseconds();
         return String.format("%d.%ds", elapsed / 1000 , elapsed % 1000);
     }
 
+    // to string total elapsed time
     public String toStringTotal() {
         long elapsed = totalMilliseconds();
         return String.format("%d.%ds", elapsed / 1000 , elapsed % 1000);
--- SnapVote/src/main/java/uct/snapvote/util/IntQueue.java
+++ SnapVote/src/main/java/uct/snapvote/util/IntQueue.java
@@ -2,12 +2,22 @@ package uct.snapvote.util;
 
 import java.util.NoSuchElementException;
 
+/**
+ * Custom queue object for primitive integers. Initialised to a fixed length but allows start and end pointer wrapping.
+ */
 public class IntQueue {
 
+    // backing data array
     private int[] data;
+    // pointers to current start and end
     private int startptr, endptr;
+    // number of elements currently in the buffer
     private int numElements;
 
+    /**
+     * Constructor
+     * @param size Number of positions in the backing array
+     */
     public IntQueue(int size)
     {
         data = new int[size];
@@ -16,16 +26,26 @@ public class IntQueue {
         endptr = 0;
     }
 
+    /**
+     * Push an item onto the back of the queue
+     * @param x the value to insert
+     */
     public void push(int x)
     {
         data[endptr] = x;
 
+        // invalid state, throw issue
         if (numElements == data.length) throw new IllegalStateException();
 
         numElements++;
+        //wrap end pointer
         if (++endptr == data.length) endptr = 0;
     }
 
+    /**
+     * Pop an item from the front of the queue
+     * @return The item at the front of the queue
+     */
     public int pop()
     {
         if (numElements == 0) throw new NoSuchElementException();
@@ -35,6 +55,10 @@ public class IntQueue {
         return p;
     }
 
+    /**
+     * Check if the queue is empty or not
+     * @return True if the queue is empty.
+     */
     public boolean isEmpty()
     {
         return numElements == 0;
